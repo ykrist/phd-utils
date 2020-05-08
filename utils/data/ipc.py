662ff56f67d43f6c84dbf737dcd898d3f2c49960
yanni_rist <yanni555rist@gmail.com>
2020-05-08 21:24:42 +1000

    finally decide on how to implement IPC

--- utils/data/ipc.py
+++ utils/data/ipc.py
@@ -3,11 +3,8 @@ Binary serialisation/deserialisation functions for interprocess communation
 """
 from .types import DARP_Data
 import msgpack
-
-def _msgpack_convert_types(x):
-    if isinstance(x, tuple):
-        return list(x)
-    raise ValueError(f"unsupported type conversion: {type(x)}")
+import dataclasses
+from oru import frozendict
 
 def serialise_DARP_Data(stream, data : DARP_Data):
     return msgpack.dump({
@@ -23,3 +20,31 @@ def serialise_DARP_Data(stream, data : DARP_Data):
         'tw_start' : { k : float(v) for k,v in data.tw_start.items() },
         'max_ride_time' : { k : float(v) for k,v in data.max_ride_time.items() },
     }, stream, default=_msgpack_convert_types, use_single_float=True, strict_types=True)
+
+@dataclasses.dataclass(frozen=True)
+class MsgPackSerialisableDataclass:
+    __slots__ = ()
+    def to_msgpack(self):
+        return dataclasses.asdict(self)
+
+    @classmethod
+    def from_msgpack(cls, data):
+        return cls(**data)
+
+def _msgpack_convert_types(x):
+    if isinstance(x, MsgPackSerialisableDataclass):
+        return x.to_msgpack()
+    if isinstance(x, frozenset):
+        return list(x)
+    elif isinstance(x, frozendict):
+        return x._dict
+    raise TypeError(f"unsupported type conversion: {type(x)}")
+
+MSGPACK_SERIALISATION_OPTS = {
+    'default' : _msgpack_convert_types
+}
+
+MSGPACK_DERIALISATION_OPTS = {
+    'use_list' : False,
+    'strict_map_key' : False
+}
